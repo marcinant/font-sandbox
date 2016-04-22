9cfc8d57c576d31199c22768e8831f567d86dbb8
Marcin Antczak <marcin.antczak@neutrico.eu>
2016-04-22 15:29:44 +0200

    font

---
+++ gulpfile.babel.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import del from 'del';
+import fs from 'fs';
+import gulp from 'gulp';
+import path from 'path';
+import pkg from './package.json';
+import plugins from 'gulp-load-plugins';
+import underscore from 'underscore.string';
+
+var $ = plugins(),
+    _ = underscore;
+
+gulp.task('serve')
+
+gulp.task('style')
+
+gulp.task('default')
