8449bfb3422c6cc96c8a63d51722324db8d9af8c
Marcin Antczak <marcin.antczak@neutrico.eu>
2016-04-26 03:39:29 +0200

    sw

---
+++ sw.js
@@ -0,0 +1,29 @@
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('fontsandbox').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        'http://fonts.gstatic.com/s/bonbon/v7/9Y-vWjaZMFbrz3brizYoHQ.woff2'
+      ]).then(function() {
+        return self.skipWaiting();
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
