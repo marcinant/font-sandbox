5a14c31585064f8c82a4e6ff6ab98a73ab9ee15c
Marcin Antczak <marcin.antczak@neutrico.eu>
2016-04-26 03:52:18 +0200

    fix js

--- assets/scripts/sw.js
+++ assets/scripts/sw.js
@@ -1,1 +1,29 @@
-// Service Worker
+importScripts('/assets/scripts/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('fontsandbox').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        'http://fonts.gstatic.com/s/bonbon/v7/9Y-vWjaZMFbrz3brizYoHQ.woff2'
+      ]).then(function() {
+        return self.skipWaiting();
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
